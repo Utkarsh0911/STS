8f6c09c343c89c3cfb6f8924dffbca9cc38048cf
Utkarsh Sharma <Utkarsh Sharma@DESKTOP-PICSNHE>
2022-07-05 16:22:52 +0530

    commit

---
+++ devops/src/main/java/com/example/devops/User.java
@@ -0,0 +1,76 @@
+package com.example.devops;
+
+public class User {
+
+	private int id;
+	private String name;
+	private String company;
+	private String baseBranch;
+	private String location;
+	private String permanentAddress;
+	private int age;
+	private String gender;
+	private String fatherName;
+	private String designation;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getBaseBranch() {
+		return baseBranch;
+	}
+	public void setBaseBranch(String baseBranch) {
+		this.baseBranch = baseBranch;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getPermanentAddress() {
+		return permanentAddress;
+	}
+	public void setPermanentAddress(String permanentAddress) {
+		this.permanentAddress = permanentAddress;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getFatherName() {
+		return fatherName;
+	}
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
+}
--- devops/src/main/java/com/example/devops/UserController.java
+++ devops/src/main/java/com/example/devops/UserController.java
@@ -8,8 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 	
 	@RequestMapping("/status/check")
-	public String getStatus() {
-		return "Working";
+	public User getStatus() {
+		User user=new User();
+		user.setAge(25);
+		user.setBaseBranch("New Delhi");
+		user.setCompany("Tata Consultancy Services");
+		user.setDesignation("Systems Engineer");
+		user.setFatherName("SK Sharma");
+		user.setId(1825676);
+		user.setLocation("Noida STP");
+		user.setName("Utkarsh Sharma");
+		user.setPermanentAddress("Meerut");
+		return user;
 	}
 
 }
